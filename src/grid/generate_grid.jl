e20930faa7df6c98d5f5cd5a666ad5c893d5c9b2
Patrick Jaap <patrick.jaap@wias-berlin.de>
2025-06-19 10:23:15 +0200

    Use parameter structs instead of parameter files
    From Julia 1.12 on, direct inclusion on parameter files as done in this project does not work any more.
    A cleaner approach is to define parameter structs and extract the necessary parameters whenever needed.

--- src/grid/generate_grid.jl
+++ src/grid/generate_grid.jl
@@ -5,19 +5,19 @@ include("grid_2D_nanotextured.jl")
 include("grid_2D_nanotextured_8p0eM7.jl")
 
  function generate_grid(;gridDim = 1, type = "nanotextured", #"planar"
-                        amplitude = 0.8e-7, parameter_file = nothing, demo_run::Bool)
+                        amplitude = 0.8e-7, parameter_set = nothing, demo_run::Bool)
 
     if gridDim == 1
-        grid = generate_grid1D(;parameter_file, demo_run = demo_run)
+        grid = generate_grid1D(;parameter_set, demo_run = demo_run)
     elseif gridDim == 2
         if type == "nanotextured"
             if amplitude < 8.0e-7
-                grid = generate_grid2D_nanotextured(amplitude = amplitude, parameter_file = parameter_file, demo_run = demo_run)
+                grid = generate_grid2D_nanotextured(amplitude = amplitude, parameter_set = parameter_set, demo_run = demo_run)
             else
-                grid = generate_grid2D_nanotextured_8p0eM7(amplitude = amplitude, parameter_file = parameter_file, demo_run = demo_run)
+                grid = generate_grid2D_nanotextured_8p0eM7(amplitude = amplitude, parameter_set = parameter_set, demo_run = demo_run)
             end
         elseif type == "planar"
-            grid = generate_grid2D_planar(parameter_file = parameter_file, demo_run = demo_run)
+            grid = generate_grid2D_planar(parameter_set = parameter_set, demo_run = demo_run)
         end
 
     end
